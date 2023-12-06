053355358666147bb0c25a7a37b3982e274dfb13
BailoMbaye <boguiss123@gmail.com>
2023-12-06 16:45:36 +0000

    Adding the page Profile

--- lib/Pages/HomePage.dart
+++ lib/Pages/HomePage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/Widget/AppBarWidget.dart';
 import 'package:food_app/Widget/CategoriesWidget.dart';
+import 'package:food_app/Widget/DrawerWidget.dart';
 import 'package:food_app/Widget/NewestItemsWidget.dart';
 import 'package:food_app/Widget/PopularItemsWidget.dart';
 
@@ -117,6 +118,29 @@ class HomePage extends StatelessWidget {
           // Newest Items Widget
           const NewestItemsWidget(),
         ],
+      ),
+      drawer: DrawerWidget(),
+      floatingActionButton: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, 3),
+            ),
+          ],
+        ),
+        child: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(
+            CupertinoIcons.cart,
+            size: 28,
+            color: Colors.red,
+          ),
+          backgroundColor: Colors.white,
+        ),
       ),
     );
   }
