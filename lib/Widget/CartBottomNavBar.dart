b2b696745bc6706cbb41b3417be4fa634b2d102f
BailoMbaye <boguiss123@gmail.com>
2024-01-06 12:48:51 +0000

    created button order now

--- lib/Widget/CartBottomNavBar.dart
+++ lib/Widget/CartBottomNavBar.dart
@@ -7,26 +7,44 @@ class CartBottomNavBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomAppBar(
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
         height: 70,
-        child: const Row(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text(
+            const Text(
               "Total: ",
               style: TextStyle(
                 fontSize: 19,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(
-              width: 15,
-            ),
-            Text(
+            const Text(
               "8 500Fr CFA",
               style: TextStyle(
                 fontSize: 19,
                 fontWeight: FontWeight.bold,
                 color: Colors.red,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red),
+                  padding: MaterialStateProperty.all(
+                    EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20,
+                    ),
+                  ),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)))),
+              child: const Text(
+                "Order now",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
               ),
             ),
           ],
